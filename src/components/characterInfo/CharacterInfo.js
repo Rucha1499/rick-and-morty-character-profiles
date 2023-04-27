2048299aa12fe38d45cfbfa3c3031a048db58929
Rucha1499 <ruchaprashantpatil@gmail.com>
2023-04-28 00:52:22 +0530

    addition of individual character page

--- src/components/characterInfo/CharacterInfo.js
+++ src/components/characterInfo/CharacterInfo.js
@@ -1,43 +1,98 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Container from '@mui/material/Container';
+import MovieCreationRoundedIcon from '@mui/icons-material/MovieCreationRounded';
 import { getCharacterInfo } from '../../utils/api';
+import styles from './CharacterInfo.module.css';
+import Navbar from '../navbar/Navbar';
+import Footer from '../footer/Footer';
 
 const CharacterInfo = () => {
   const { id } = useParams();
 
+  // eslint-disable-next-line no-unused-vars
   const [characterInfo, setCharacterInfo] = useState();
+  const [loading, setIsloading] = useState(true);
 
   const getCharacterInformation = async () => {
     const data = await getCharacterInfo(id);
     setCharacterInfo(data);
+    setIsloading(false);
   };
 
   useEffect(() => {
-    if (id) {
-      getCharacterInformation();
-    }
+    getCharacterInformation();
   }, [id]);
 
   return (
-    <div>
-      {characterInfo ? (
-        <>
-          Character Information :
-          {' '}
-          -
-          {' '}
-          {characterInfo.species}
-          {' '}
-          -
-          {' '}
-          {characterInfo.name}
-          {' '}
-          -
-          {' '}
-          {characterInfo.id}
-        </>
-      ) : ''}
-    </div>
+    <>
+      <Navbar />
+      <Container fixed>
+        {loading ? '' : (
+          <div className={styles.detailsPage}>
+            <div className={styles.personalInfo}>
+              <div className={styles.displayPicture}>
+                <img src={characterInfo.image} alt="display" />
+                <p>{characterInfo.name}</p>
+              </div>
+              <div className={styles.divider} />
+              <p>DETAILS</p>
+              <ul>
+                <li>
+                  Status :
+                  {' '}
+                  {characterInfo.status}
+                  {' '}
+                </li>
+                <li>
+                  Gender :
+                  {' '}
+                  {characterInfo.gender}
+                  {' '}
+                </li>
+                <li>
+                  Species :
+                  {' '}
+                  {characterInfo.species}
+                  {' '}
+                </li>
+                <li>
+                  Type :
+                  {' '}
+                  {characterInfo.Type || 'Unknown'}
+                  {' '}
+                </li>
+                <li>
+                  Origin :
+                  {' '}
+                  {characterInfo.origin.name}
+                  {' '}
+                </li>
+                <li>
+                  Location :
+                  {' '}
+                  {characterInfo.location.name}
+                  {' '}
+                </li>
+              </ul>
+            </div>
+            <div className={styles.episodes}>
+              <div className={styles.episodeHeader}>FEATURED IN</div>
+              {characterInfo.episode && characterInfo.episode.length > 0
+                ? characterInfo.episode.map((episode) => (
+                  <div className={styles.episodeContainer}>
+                    <MovieCreationRoundedIcon fontSize="small" sx={{ color: '#57C5B6' }} />
+                    <p key={episode}>
+                      {episode}
+                    </p>
+                  </div>
+                )) : ''}
+            </div>
+          </div>
+        )}
+      </Container>
+      <Footer />
+    </>
   );
 };
 
